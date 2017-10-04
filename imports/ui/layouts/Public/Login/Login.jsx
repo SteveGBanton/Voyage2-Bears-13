ddcaad69c8ddf26ba02b142340a73c4fd86a31b5
Brandon Mellus <mellusbrandon@gmail.com>
2017-10-04 16:05:56 -0400

    Code Styling: layouts/Public

--- imports/ui/layouts/Public/Login/Login.jsx
+++ imports/ui/layouts/Public/Login/Login.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 // import { Row, Col, FormGroup, ControlLabel, Button } from 'react-bootstrap';
-//import validate from '../../../modules/validate';
+// import validate from '../../../modules/validate';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 import { Bert } from 'meteor/themeteorchef:bert';
 
-import OAuthLoginButtons from '../../../components/OAuthLoginButtons/OAuthLoginButtons.jsx';
-import customFormValidator from '../../../../modules/custom-form-validator';
-
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+
+import OAuthLoginButtons from '../../../components/OAuthLoginButtons/OAuthLoginButtons.jsx';
+import customFormValidator from '../../../../modules/custom-form-validator';
 
 const rules = {
   emailAddress: {
@@ -20,7 +20,7 @@ const rules = {
   password: {
     required: true,
   },
-}
+};
 
 const messages = {
   emailAddress: {
@@ -30,7 +30,7 @@ const messages = {
   password: {
     required: 'Please enter your password.',
   },
-}
+};
 
 class Login extends React.Component {
   constructor(props) {
@@ -43,25 +43,22 @@ class Login extends React.Component {
         password: "",
         emailAddress: "",
       },
-    })
+    });
   }
 
   formValidate() {
-
     const input = {
       emailAddress: this.emailAddress.input.value,
       password: this.password.input.value,
-    }
+    };
 
-    let formErrors = customFormValidator(input, rules, messages);
+    const formErrors = customFormValidator(input, rules, messages);
 
     if (!formErrors) {
       this.handleSubmit();
     } else {
-      this.setState({formErrors});
+      this.setState({ formErrors });
     }
-
-    return;
   }
 
   handleSubmit() {
@@ -84,44 +81,42 @@ class Login extends React.Component {
 
         <form onSubmit={event => event.preventDefault()}>
 
-        <TextField
-          name="username"
-          floatingLabelText="Email Address"
-          ref={input => (this.emailAddress = input)}
-          errorText={this.state.formErrors.emailAddress}
-        /><br/>
+          <TextField
+            name="username"
+            floatingLabelText="Email Address"
+            ref={(input) => { this.emailAddress = input; }}
+            errorText={this.state.formErrors.emailAddress}
+          /> <br />
 
-        <TextField
-          name="password"
-          type="password"
-          floatingLabelText="Password"
-          ref={password => (this.password = password)}
-          errorText={this.state.formErrors.password}
-        />
+          <TextField
+            name="password"
+            type="password"
+            floatingLabelText="Password"
+            ref={(password) => { this.password = password; }}
+            errorText={this.state.formErrors.password}
+          />
 
-        <RaisedButton
-          type="submit"
-          fullWidth={true}
-          onClick={this.formValidate}
-          style={{margin: "35px 0 20px 0"}}
-        >
-          Log In
-        </RaisedButton>
+          <RaisedButton
+            type="submit"
+            fullWidth
+            onClick={this.formValidate}
+            style={{ margin: "35px 0 20px 0" }}
+          >
+            Log In
+          </RaisedButton>
 
         </form>
 
 
-    </div>);
+      </div>);
   }
 }
 
 Login.propTypes = {
-  history: PropTypes.object.isRequired,
+  history: PropTypes.shape({}).isRequired,
 };
 
 export default Login;
-
-
 
 /* <Row>
   <Col xs={12} sm={6} md={5} lg={4}>
