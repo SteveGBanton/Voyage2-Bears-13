ea6fb4be98a0fb1dd8fce7ce01fe98cd74b2e058
Brandon Mellus <mellusbrandon@gmail.com>
2017-10-05 18:07:11 -0400

    Update method for Learning Paths

--- imports/api/LearningPath/methods.js
+++ imports/api/LearningPath/methods.js
@@ -15,20 +15,48 @@ const learningPathsInsert = new ValidatedMethod({
     description: { type: String },
     skills: { type: Array, minCount: 1 },
     'skills.$': { type: String },
-    resources: { type: Array, minCount: 1 },
+    resource: { type: Array, minCount: 1 },
     'resources.$': { type: resourceSchema },
   }).validator(),
   run(lp) {
     // Change when code for mentor privileges
-    return LearningPaths.insert({ mentor: this.userId, ...lp });
+    try {
+      return LearningPaths.insert({ mentor: this.userId, ...lp });
+    } catch (exception) {
+      throw new Meteor.Error(
+        'learning-paths.insert.error',
+        `Error inserting new Learning Path: ${exception}`,
+      );
+    }
   },
 });
 
-export { learningPathsInsert };
+const learningPathsUpdate = new ValidatedMethod({
+  name: 'learning-paths.update',
+  validate: new SimpleSchema({
+    _id: { type: String, regEx: SimpleSchema.RegEx.Id },
+    title: { type: String },
+    description: { type: String },
+    skills: { type: Array },
+    'skills.$': { type: String },
+    resource: { type: Array },
+    'resources.$': { type: resourceSchema },
+  }).validator(),
+  run(lp) {
+    try {
+      return LearningPaths.update({ _id: lp._id }, { $set: lp });
+    } catch (exception) {
+      console.log("Problem with updating Learning Path:", err);
+    }
+  },
+});
+
+export { learningPathsInsert, learningPathsUpdate };
 
 rateLimit({
   methods: [
     'learning-paths.insert',
+    'learning-paths.update',
   ],
   limit: 5,
   timeRange: 1000,
