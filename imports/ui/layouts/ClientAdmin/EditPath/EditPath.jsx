ee522eb83c8aa4f439db8cd659ee83af3c5c8f50
Steve Banton <steve.g.banton@gmail.com>
2017-10-23 12:07:53 -0400

    Added functional Edit Path form
    Grabs path from database based on id in URL, passes to
    LearningPathEditor.

--- imports/ui/layouts/ClientAdmin/EditPath/EditPath.jsx
+++ imports/ui/layouts/ClientAdmin/EditPath/EditPath.jsx
@@ -4,35 +4,33 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 import RaisedButton from 'material-ui/RaisedButton';
 
+import LearningPaths from '../../../../api/LearningPath/LearningPath';
 import Loading from '../../../components/Loading/Loading';
+import LearningPathEditor from '../../../components/LearningPathEditor/LearningPathEditor';
 
 class EditPath extends React.Component {
 
   constructor(props) {
     super(props);
-
   }
 
   render() {
-    const { loading, learningPathDoc, history, isUserOwner } = this.props;
+    const { loading, learningPathDoc, history } = this.props;
     return (
       // Make sure user owns document, or else cannot edit it.
-        (!loading && isUserOwner) ?
+        (!loading) ?
           <div className="edit-path">
             <h1>Edit A Learning Path</h1>
 
-            {/* TODO
-
-              Form To Edit Paths
-
-              Call:
-
-              <LearningPathEditor learningPath={learningPathDoc}/>
-
-            */}
+            {(learningPathDoc && learningPathDoc.mentor === Meteor.userId()) ?
+              <LearningPathEditor path={learningPathDoc} history={history} />
+              :
+              'Sorry, only the creator can edit this Learning Path.'
+            }
 
           </div>
-          : ''
+          :
+          <Loading />
     );
   }
 }
@@ -47,13 +45,15 @@ EditPath.propTypes = {
 
 
 export default createContainer(({ match }) => {
+  // const subscriptionAll = Meteor.subscribe('learning-paths');
+  // console.log(LearningPaths.find({}).fetch());
 
-  // Load document to edit from DB using URL.
-  // /learning-path/:learningPathId/edit
-  // Also determine whether user is owner of the doc.
+  const pathId = match.params.learningPathId;
+  const subscription = Meteor.subscribe('learning-paths.view', pathId);
+  const learningPathDoc = LearningPaths.findOne(pathId);
 
   return {
-    // learningPathDoc: learningPathDoc,
-    // isUserOwner: isUserOwner,
+    loading: !subscription.ready(),
+    learningPathDoc,
   };
 }, EditPath);
