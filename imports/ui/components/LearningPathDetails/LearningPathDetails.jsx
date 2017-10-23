a07c81208b1a2516c6be9f505792fbadba0bd486
Brandon Mellus <mellusbrandon@gmail.com>
2017-10-23 07:35:48 -0400

    Create LearningPathDetails component

---
+++ imports/ui/components/LearningPathDetails/LearningPathDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class LearningPathDetails extends React.Component {
+  render() {
+    return (
+      <div className="learning-path-details"></div>
+    );
+  }
+}
