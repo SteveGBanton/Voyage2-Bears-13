d3ca34a6a2329e200bc86628e8f5faad7a5553db
Brandon Mellus <mellusbrandon@gmail.com>
2017-11-02 12:17:45 -0400

    Style LearningPathDetails

--- imports/ui/components/LearningPathDetails/LearningPathDetails.jsx
+++ imports/ui/components/LearningPathDetails/LearningPathDetails.jsx
@@ -1,34 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card, { CardHeader, CardMedia, CardTitle, CardActions, CardText } from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
 import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
+import { green500, red500 } from 'material-ui/styles/colors';
 import { createContainer } from 'meteor/react-meteor-data';
+
+import './LearningPathDetails.scss';
+
+const DESCRIPTION_LENGTH = 100;
 
 class LearningPathDetails extends React.Component {
   render() {
     const { _id, title, mentorName, description, thumbnail, aggregatedVotes } = this.props;
     return (
-      <Card key={_id} className="learning-path-card">
-        <div className="learning-path-details">
+      <Card key={_id} className="lp-details">
+        <div className="lp-container">
           <CardHeader className="lp-header">
-            <CardMedia overlay={(<p>{description}</p>)}>
-              <img className="learning-path-thumbnail" src={thumbnail} alt={title} />
+            <CardMedia className="lp-media">
+              <img className="lp-thumbnail" src={thumbnail} alt={title} />
             </CardMedia>
           </CardHeader>
+          <Divider />
           <div className="lp-content">
-            <CardTitle title={title} />
-            <CardActions>
-              <IconButton tooltip="Upvote!">
-                <FontIcon className="fa fa-thumbs-o-up" />
-              </IconButton>
-              <span className="lp-votes">{aggregatedVotes}</span>
-              <IconButton tooltip="Downvote...">
-                <FontIcon className="fa fa-thumbs-o-down" />
-              </IconButton>
+            <CardTitle className="lp-title" title={title} />
+            <CardText className="lp-description">{
+              description.length > DESCRIPTION_LENGTH ?
+                `${description.slice(0, DESCRIPTION_LENGTH)}...` :
+                description
+            }</CardText>
+            <CardActions className="lp-votes">
+              <IconButton
+                className="lp-vote-btn lp-upvote"
+                tooltip="Upvote!"
+                iconClassName="fa fa-thumbs-o-up"
+                iconStyle={{
+                  iconHoverColor: green500,
+                }}
+              />
+              <span className="lp-vote-count">{aggregatedVotes}</span>
+              <IconButton
+                className="lp-vote-btn lp-downvote"
+                tooltip="Downvote..."
+                iconClassName="fa fa-thumbs-o-down"
+                iconStyle={{
+                  iconHoverColor: red500,
+                }}
+              />
             </CardActions>
             {/* A link that points to the mentor's user page */}
-            <CardText>{/* <a href="#"> */}{mentorName}{/* </a> */}</CardText>
+            <div className="lp-mentor">
+              <CardText className="lp-mentor-name">
+                {/* <a href="#"> */}{mentorName}{/* </a> */}
+              </CardText>
+            </div>
+            <div className="clear" />
           </div>
         </div>
       </Card>
--- imports/ui/layouts/AllUserAccess/LearningPaths/LearningPaths.jsx
+++ imports/ui/layouts/AllUserAccess/LearningPaths/LearningPaths.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Random } from 'meteor/random';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import RaisedButton from 'material-ui/RaisedButton';
 
 import Loading from '../../../components/Loading/Loading';
+import LearningPathDetails from '../../../components/LearningPathDetails/LearningPathDetails';
 
 class LearningPaths extends React.Component {
 
@@ -20,7 +22,15 @@ class LearningPaths extends React.Component {
             <h1>All Learning Paths</h1>
 
             {/* TODO Create learning path categories  */}
-
+            <LearningPathDetails
+            lp={{
+              _id: Random.id(),
+              title: 'Example title',
+              mentor: Random.id(),
+              description: 'This is a long long long long long long long long long long long long long long long long long long long description for this learning path',
+              thumbnail: 'https://cdn.pixabay.com/photo/2017/10/26/20/00/pumpkin-2892303_960_720.jpg',
+              aggregatedVotes: 0,
+            }} />
             {/* TODO Add categories users can browse */}
 
           </div>
@@ -31,10 +41,10 @@ class LearningPaths extends React.Component {
 
 // TODO edit proptypes
 LearningPaths.propTypes = {
-  loading: PropTypes.bool.isRequired,
-  doc: PropTypes.object,
-  match: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired,
+  // loading: PropTypes.bool.isRequired,
+  // doc: PropTypes.object,
+  // match: PropTypes.object.isRequired,
+  // history: PropTypes.object.isRequired,
 };
 
 export default createContainer(({ match }) => {
