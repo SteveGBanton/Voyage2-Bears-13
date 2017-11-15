56764be9ccbefdd859b7b35b0787ab8ff670fbe2
Steve Banton <steve.g.banton@gmail.com>
2017-11-15 12:55:05 -0500

    Edit LearningPathView component, styling

--- imports/ui/layouts/AllUserAccess/LearningPathView/LearningPathView.jsx
+++ imports/ui/layouts/AllUserAccess/LearningPathView/LearningPathView.jsx
@@ -16,18 +16,6 @@ import LearningPathCollection from '../../../../api/LearningPath/LearningPath';
 
 if (Meteor.isClient) import './LearningPathView.scss';
 
-
-// "_id": "HF7L4NSkTbuKg7Rgi",
-//   "title": "Use Asana to track your team’s work & manage projects · Asana",
-//     "description": "It’s free to use, simple to get started, and powerful enough to run your entire business. Sign up for free today.",
-//       "url": "http://www.asana.com",
-//         "thumbnail": "https://luna1.co/906e13.png",
-//           "createdAt": "2017-11-13T16:08:58.353Z",
-//             "updatedAt": "2017-11-13T21:52:31.310Z"
-//     }
-//   ],
-
-
 const LearningPathView = ({ loading, learningPathDoc, history, user }) => (
   !loading ?
     <div className="learning-path-view-container">
@@ -35,37 +23,42 @@ const LearningPathView = ({ loading, learningPathDoc, history, user }) => (
         <div className="title">
           <h1>{learningPathDoc.title}</h1>
 
-          {(user && user.savedLearningPaths[learningPathDoc._id]) ?
-            <div className="save-chip">
+          <div className="save-edit-chips">
+            {(user && user.savedLearningPaths[learningPathDoc._id]) ?
+                <Chip
+                  onClick={() => Meteor.call('users.toggleSaveLearningPath', { learningPathId: learningPathDoc._id }, (err) => {
+                    if (err) Bert.alert('err.reason', 'danger')
+                  })}
+                  backgroundColor="#FFC0CB"
+                  labelColor="#424242"
+                >
+                  <Avatar color="red" backgroundColor="#FFB6C1" icon={<FontIcon className="material-icons">favorite</FontIcon>} />
+                  Saved
+                </Chip>
+              :
+              (user) ?
+                <Chip
+                  onClick={() => Meteor.call('users.toggleSaveLearningPath', { learningPathId: learningPathDoc._id }, (err) => {
+                    if (err) Bert.alert('err.reason', 'danger')
+                  })}
+                >
+                  <Avatar color="#FFF" icon={<FontIcon className="material-icons">favorite</FontIcon>} />
+                  Save
+                </Chip>
+                :
+                ''
+            }
+            {(user && learningPathDoc.mentor === user._id) ?
               <Chip
-                onClick={() => Meteor.call('users.toggleSaveLearningPath', { learningPathId: learningPathDoc._id })}
-                backgroundColor="#FFC0CB"
-                labelColor="#424242"
+                onClick={() => history.push(`/learning-path/${learningPathDoc._id}/edit`)}
               >
-                <Avatar color="red" backgroundColor="#FFB6C1" icon={<FontIcon className="material-icons">favorite</FontIcon>} />
-                Saved
+                <Avatar color="#FFF" icon={<FontIcon className="material-icons">edit</FontIcon>} />
+                Edit Path
               </Chip>
-            </div>
-            :
-            <div className="save-chip">
-            <Chip
-              onClick={() => Meteor.call('users.toggleSaveLearningPath', { learningPathId: learningPathDoc._id })}
-            >
-              <Avatar color="#FFF" icon={<FontIcon className="material-icons">favorite</FontIcon>} />
-              Save
-            </Chip>
-            </div>
-          }
-          {(user && learningPathDoc.mentor === user._id) ?
-            <Chip
-              onClick={() => history.push(`/learning-path/${learningPathDoc._id}/edit`)}
-            >
-              <Avatar color="#FFF" icon={<FontIcon className="material-icons">edit</FontIcon>} />
-              Edit Path
-            </Chip>
-            :
-            ''
-          }
+              :
+              ''
+            }
+          </div>
         </div>
 
         <Paper className="paper-box">
@@ -101,7 +94,7 @@ const LearningPathView = ({ loading, learningPathDoc, history, user }) => (
                   <div><img width="50" src={resource.thumbnail} /></div>
                 </div>
                 <div className="description">{resource.description}</div>
-                
+
                 <div className="link-and-mark">
                   <a className="link" href={resource.url} target="_blank">
                     <Chip
@@ -111,7 +104,7 @@ const LearningPathView = ({ loading, learningPathDoc, history, user }) => (
                       <Avatar color="#FFF" icon={<FontIcon className="material-icons">link</FontIcon>} />
                       Resource Link
                     </Chip>
-                  
+
                   </a>
                   {(user && user.completedResources &&
                     user.completedResources[learningPathDoc._id] &&
@@ -144,7 +137,7 @@ const LearningPathView = ({ loading, learningPathDoc, history, user }) => (
                     </Chip>
                     </div>
                     :
-                    user ? 
+                    user ?
                       <div className="save-chip">
                         <Chip
                           onClick={() => Meteor.call('users.toggleCompletedResource',
@@ -173,9 +166,9 @@ const LearningPathView = ({ loading, learningPathDoc, history, user }) => (
                       ''
                   }
                 </div>
-                
+
                 {((index + 1) !== learningPathDoc.resources.length) ?
-                  <Divider style={{ marginTop: 50, width: '100%' }} /> 
+                  <Divider style={{ marginTop: 50, width: '100%' }} />
                   :
                   ''
                 }
