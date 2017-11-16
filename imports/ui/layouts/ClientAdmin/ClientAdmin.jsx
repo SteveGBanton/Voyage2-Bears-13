212bf471a6804c8a7670ef586ee53311e7e9e58b
Steve Banton <steve.g.banton@gmail.com>
2017-11-16 11:10:29 -0500

    Remove logout - redirect to home page instead.

--- imports/ui/layouts/ClientAdmin/ClientAdmin.jsx
+++ imports/ui/layouts/ClientAdmin/ClientAdmin.jsx
@@ -38,7 +38,7 @@ export default class ClientAdmin extends React.Component {
     const usernameAskOrLogout = (authenticated && !user.username) ?
       <AddUsername />
       :
-      (<Redirect to="/logout" />);
+      (<Redirect to="/" />);
     return (
       <div className="dashboard">
 
